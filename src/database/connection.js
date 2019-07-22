6ed46bf7667fbbdb52871ef3fc8423880c208e26
Iago <yagomello16@gmail.com>
2019-07-22 17:46:37 -0300

    added UseFindAndModify mongoOptions

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,8 +3,12 @@ let Mongoose = require('mongoose')
 const mongoUsr = process.env.MONGO_ATLAS_USER
 const mongoPw = process.env.MONGO_ATLAS_PW
 const bdName = process.env.MONGO_ATLAS_BDNAME
-const mongoOptions = { useNewUrlParser: true }
 const mongoURI = `mongodb+srv://${mongoUsr}:${mongoPw}@cluster0-qnlg0.mongodb.net/${bdName}?retryWrites=true&w=majority`
+
+const mongoOptions = {
+  useNewUrlParser: true,
+  useFindAndModify: false
+}
 
 Mongoose.connect(mongoURI, mongoOptions, err => {
   if (err) {
